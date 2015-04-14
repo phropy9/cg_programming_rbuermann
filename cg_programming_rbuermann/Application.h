100af9fd1ab5173fe7818b2884cd62112aa95bad
unknown <r202-stu@AI241LAB0202P05.aipdcls.edu>
2015-04-13 21:48:02 -0700

    Got my project up and running

--- cg_programming_rbuermann/Application.h
+++ cg_programming_rbuermann/Application.h
@@ -5,15 +5,47 @@
 
 #pragma once
 
+//#include <glew.h>
+#include <GL\glew.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
+
+
+//Standard Headers...
 #include <stdio.h>
+#include <memory.h>
 #include <stdlib.h>
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <cmath>
-#include <GL/glew.h>
+
+
+
 
 //Name Spaces...
 
 using namespace std;
+using namespace glm;
 
+#define GLFW_DLL
+#define APP_NAME "<<buermannRoman>>"
+#define EXIT_WITH_ERROR -1
+#define EXIT_WITH_SUCCESS 0
+#define OPEN_GL_VERSION 3
+#define ANTIALIASING 4
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define GLFW_FAIL  0
+
+extern GLFWwindow* window;
+extern GLint shaderStatus;
+extern bool activeRef;
+
+
+
+//int Load();
+//void Update();
+//void Render();
+
+//void CheckShaderStatus();
